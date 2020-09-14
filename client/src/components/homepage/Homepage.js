7cef46ccb4d7c31e7d006c459d64a6569b41dfc6
valerian <you@example.com>
2020-09-14 16:34:02 +0200

    homepage componenent ok

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -5,7 +5,6 @@ import { path } from "../index";
 const Homepage = () => {
   const [carroussel, setCarrousel] = useState([]);
 
-
   useEffect(() => {
     const fetchCarrousselImg = async () => {
       Promise.all([
@@ -18,11 +17,8 @@ const Homepage = () => {
         .then((urlImg) => setCarrousel(urlImg));
     };
 
-
     fetchCarrousselImg();
   }, []);
-
-
 
   const sliderButton = (url) => {
     const img = document.querySelector("img[id=imgSlider]");
@@ -31,7 +27,11 @@ const Homepage = () => {
   };
 
   return (
+    
+
     <div className={Style.homepageMainContainer}>
+
+        {/*Slider img and buttons */}
       <div>
         <img
           className={Style.imgSlider}
@@ -40,37 +40,56 @@ const Homepage = () => {
           id="imgSlider"
         />
         <div className={Style.sliderContainer}>
-        {carroussel
-          ? carroussel.map((imgUrl, index) => (
-              <div
-              key={index}
-                className={Style.sliders}
-                onClick={() => sliderButton(imgUrl)}
-              ></div>
-            ))
-          : null}
-      </div>
+          {carroussel
+            ? carroussel.map((imgUrl, index) => (
+                <div
+                  key={index}
+                  className={Style.sliders}
+                  onClick={() => sliderButton(imgUrl)}
+                ></div>
+              ))
+            : null}
+        </div>
       </div>
 
+      {/*article and homepage message*/}
+
       <div className={Style.homeMessage}>
+        {/*Homepage message */}
+
         <h1>Inspirations recettes, bonjour !</h1>
-        <p>Toujours plus d’inspiration en cuisine grâce à nos recettes faciles,
-        rapides et tendances.</p>
+        <p>
+          Toujours plus d’inspiration en cuisine grâce à nos recettes faciles,
+          rapides et tendances.
+        </p>
       </div>
-      <div className={Style.homepageImgContainer}>
-        <div>
-          <img className={Style.homepageImg} src={`${path}image/homepageImg1.jpg`} alt="article img"/>
-          <div>Decouvrez les meilleurs recettes de salades</div>
+
+       {/*articles*/}
+       <div className={Style.homepageArticlesContainer}>
+        <div className={Style.articleContainer}>
+          <img
+            src={`${path}image/homepageImg1.jpg`}
+            className={Style.articleImg}
+          />
+          <p>Les legumes</p>
         </div>
-        <div>
-          <img className={Style.homepageImg} src={`${path}image/homepageImg2.jpeg`} alt="article img"/>
-          <div> Decouvrez les meilleurs recettes de poisson</div>
+        <div className={Style.articleContainer}>
+          <img
+            src={`${path}image/homepageImg2.jpeg`}
+            className={Style.articleImg}
+          />
+          <p>les poissons</p>
         </div>
-        <div>
-          <img className={Style.homepageImg} src={`${path}image/homepageImg3.jpg`} alt="article img"/>
-          <div>Decouvrez les meilleurs recettes de soupes</div>
+        <div className={Style.articleContainer}>
+          <img
+            src={`${path}image/homepageImg3.jpg`}
+            className={Style.articleImg}
+          />
+          <p>les soupes</p>
         </div>
       </div>
+
+     
     </div>
   );
 };
