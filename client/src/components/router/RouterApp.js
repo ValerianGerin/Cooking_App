7c69adfa87e6efdd88954be410d3f906e61f69c5
valerian <you@example.com>
2020-09-21 15:55:35 +0200

    adding a new route

--- client/src/components/router/RouterApp.js
+++ client/src/components/router/RouterApp.js
@@ -1,14 +1,16 @@
 import React from "react"
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import {Homepage, RecipeList, RecipeDetail, AddForm, UpdateForm} from "../index"
 
 const RouterApp = () =>{
   return <>
-    <Route exact path="/" component={Homepage}></Route>
+   <Switch>
+   <Route exact path="/" component={Homepage}></Route>
     <Route exact path="/recipes" component={RecipeList}></Route>
-    <Route exact path="/recipes/:id" component={RecipeDetail}></Route>
+    {/** <Route exact path="/recipes/:id" component={RecipeDetail}></Route>*/}
     <Route exact path="/recipes/new" component={AddForm}></Route>
     <Route exact path="/update/:id" component={UpdateForm}></Route>
+   </Switch>
   </>
 }
 
