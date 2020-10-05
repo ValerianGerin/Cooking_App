18ee26db81e2b630741c5d852f65f581636264b9
valerian <you@example.com>
2020-10-05 11:25:41 +0200

    first layout for the RecipeList component

--- client/src/components/recipes/recipe-list/RecipeList.js
+++ client/src/components/recipes/recipe-list/RecipeList.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {Link} from "react-router-dom"
+import path from "../../../assets/config/path";
+import Style from "./RecipeList.module.css";
 
-const RecipeList = ()=>{
-  return (<div>RecipeList</div>)
-}
+const RecipeList = () => {
+  const [recipeList, setRecipeList] = useState();
+
+  useEffect(() => {
+    const fetchRecipeList = async () => {
+      const recipes = await fetch(`${path}recipes`);
+      const response = await recipes.json();
+      setRecipeList(response);
+    };
+    fetchRecipeList();
+  }, []);
+
+  const deleteRecipe = (id) =>{
+    fetch(`${path}recipes/delete/${id}`, {method:"delete"})
+  }
+ 
+
+  return (
+    <div className={Style.recipesMainContainer}>
+      {recipeList && recipeList.length > 0
+        ? recipeList.map((recipe, index) => (
+            <div key={index} className={Style.recipeContainer}>
+              <div className={Style.recipeInfos}>
+                <div>
+                  <img src={recipe.img}/>
+                </div>
+                <div className={Style.recipeText}>
+                  <p>{recipe.title}</p>
+                  <p>{recipe.description}</p>
+                 
+                </div>
+              </div>
+              <div className={Style.buttonContainer}>
+                <button onClick={()=>deleteRecipe(recipe._id)} className={Style.button}>Supprimer</button>
+                <button className={Style.button}>Modifier</button>
+              </div>
+            </div>
+        ))
+        : <div className={Style.norecipe}>Pas de recettes, ajouter en au moins une pour consulter la liste</div>}
+    </div>
+  );
+};
 
 export default RecipeList;
