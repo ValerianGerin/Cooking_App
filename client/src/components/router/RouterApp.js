62dd9b75dadcac6f552b6511403a5f7c54186d7d
valerian <you@example.com>
2020-10-05 11:40:09 +0200

    update path order

--- client/src/components/router/RouterApp.js
+++ client/src/components/router/RouterApp.js
@@ -1,18 +1,25 @@
-import React from "react"
-import {Route, Switch} from "react-router-dom";
-import {Homepage, RecipeList, RecipeDetail, AddForm, UpdateForm} from "../index"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import {
+  Homepage,
+  RecipeList,
+  RecipeDetail,
+  AddForm,
+  UpdateForm,
+} from "../index";
 
-const RouterApp = () =>{
-  return <>
-   <Switch>
-   <Route exact path="/" component={Homepage}></Route>
-    <Route exact path="/recipes" component={RecipeList}></Route>
-    {/** <Route exact path="/recipes/:id" component={RecipeDetail}></Route>*/}
-    <Route exact path="/recipes/new" component={AddForm}></Route>
-    <Route exact path="/update/:id" component={UpdateForm}></Route>
-   </Switch>
-  </>
-}
-
+const RouterApp = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Homepage}></Route>
+        <Route exact path="/recipes" component={RecipeList}></Route>
+        <Route exact path="/recipes/new" component={AddForm}></Route>
+        <Route exact path="/recipes/:id" component={RecipeDetail}></Route>
+        <Route exact path="/update/:id" component={UpdateForm}></Route>
+      </Switch>
+    </>
+  );
+};
 
 export default RouterApp;
