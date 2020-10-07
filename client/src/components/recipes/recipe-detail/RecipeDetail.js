52821d3a1faffe70d61ddd89d6dfcf2573d23a72
valerian <you@example.com>
2020-10-07 11:45:07 +0200

    First features for RecipeDetail component

--- client/src/components/recipes/recipe-detail/RecipeDetail.js
+++ client/src/components/recipes/recipe-detail/RecipeDetail.js
@@ -3,25 +3,39 @@ import Style from "./RecipeDetail.module.css";
 import path from "../../../assets/config/path";
 
 const RecipeDetail = ({ match }) => {
-
   const {
     params: { id },
   } = match;
 
   const [recipeDetail, setRecipeDetail] = useState();
 
-  useEffect(()=>{
-    const fetchRecipeDetail = async ()=>{
+  useEffect(() => {
+    const fetchRecipeDetail = async () => {
       const data = await fetch(`${path}recipes/${id}`);
-      const response = await data.json()
-      setRecipeDetail(response)
-    }
-    fetchRecipeDetail()
-  }, [])
+      const response = await data.json();
+      setRecipeDetail(response);
+    };
+    fetchRecipeDetail();
+  }, []);
 
-
-  
-  return <div>RecipeDetail</div>;
+  return (
+    <div className={Style.mainContainer}>
+      <div className={Style.mainInfosRecipeContainer}>
+        <div className={Style.recipeImg}>
+          <img />
+        </div>
+        <div className={Style.recipeInfos}>
+          <span>title</span>
+          <span>description</span>
+          <span>crée le</span>
+        </div>
+      </div>
+      <div className={Style.recipeIngredients}>
+        <div>Liste des ingredients</div>
+      </div>
+      <div className={Style.recipePreparation}>conseil de preparation</div>
+    </div>
+  );
 };
 
 export default RecipeDetail;
