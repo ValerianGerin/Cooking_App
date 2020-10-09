2c1f7e07219909b84ad665f52fc94f6e972c420e
valerian <you@example.com>
2020-10-09 15:03:48 +0200

    adding some padding and correct the link bug on the whole component

--- client/src/components/recipes/recipe-list/RecipeList.js
+++ client/src/components/recipes/recipe-list/RecipeList.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import path from "../../../assets/config/path";
 import Style from "./RecipeList.module.css";
 
 const RecipeList = () => {
   const [recipeList, setRecipeList] = useState();
-  const [reload, setReload] = useState(false)
+  const [reload, setReload] = useState(false);
 
   useEffect(() => {
     const fetchRecipeList = async () => {
@@ -16,35 +16,44 @@ const RecipeList = () => {
     fetchRecipeList();
   }, [reload]);
 
-  const deleteRecipe = (id) =>{
-    fetch(`${path}recipes/delete/${id}`, {method:"delete"})
-    setReload(!reload)
- 
-  }
-
+  const deleteRecipe = (id) => {
+    fetch(`${path}recipes/delete/${id}`, { method: "delete" });
+    setReload(!reload);
+  };
 
   return (
     <div className={Style.recipesMainContainer}>
-      {recipeList && recipeList.length > 0
-        ? recipeList.map((recipe, index) => (
-            <Link to={`recipes/${recipe._id}`} key={index} className={Style.recipeContainer}>
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((recipe, index) => (
+          <div className={Style.recipeContainer}>
+            <Link to={`recipes/${recipe._id}`} key={index} className={Style.link}>
               <div className={Style.recipeInfos}>
                 <div>
-                  <img src={recipe.img} alt={recipe.title}/>
+                  <img src={recipe.img} alt={recipe.title} />
                 </div>
                 <div className={Style.recipeText}>
                   <p>{recipe.title}</p>
                   <p>{recipe.description}</p>
-                 
                 </div>
               </div>
-              <div className={Style.buttonContainer}>
-                <button onClick={()=>deleteRecipe(recipe._id)} className={Style.button}>Supprimer</button>
-                <button className={Style.button}>Modifier</button>
-              </div>
             </Link>
+
+            <div className={Style.buttonContainer}>
+              <button
+                onClick={() => deleteRecipe(recipe._id)}
+                className={Style.button}
+              >
+                Supprimer
+              </button>
+              <button className={Style.button}>Modifier</button>
+            </div>
+          </div>
         ))
-        : <div className={Style.norecipe}>Pas de recettes, ajouter en au moins une pour consulter la liste</div>}
+      ) : (
+        <div className={Style.norecipe}>
+          Pas de recettes, ajouter en au moins une pour consulter la liste
+        </div>
+      )}
     </div>
   );
 };
